0b373314d62316b4e4d66ee30d8315451bb95d5b
massimorea <82827248+massimorea@users.noreply.github.com>
2025-02-21 18:03:56 +0100

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-# app.py
 from flask import Flask
+import rendimenti_asset  # Se vuoi richiamare funzioni dal tuo script
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>QUANT-REA</h1><p>Ecco il nostro endpoint web su Heroku!</p>"
+    # Esegui qualche funzione definita in rendimenti-asset.py
+    # result = rendimenti_asset.calcola_qualcosa()
+    return "<h1>Home QUANT-REA</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
