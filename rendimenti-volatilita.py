40c31b20a5ed3709b94fbe63ba4047e467989b04
massimorea <82827248+massimorea@users.noreply.github.com>
2025-02-22 02:01:52 +0100

    rendimenti-volatilita.py

--- rendimenti-volatilita.py
+++ rendimenti-volatilita.py
@@ -53,4 +53,5 @@ def run_analysis():
     return "Analisi della volatilità avviata!", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)), debug=True)
+
