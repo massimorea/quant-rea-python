12d339f2bb50f2d45db8d0854215342122ee005b
massimorea <82827248+massimorea@users.noreply.github.com>
2025-02-22 01:40:42 +0100

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask
-#import rendimenti-asset  # Se vuoi richiamare funzioni dal tuo script
-import rendimenti_volatilita  # Se vuoi richiamare funzioni dal tuo script
+from flask import Flask, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    # Esegui qualche funzione definita in rendimenti-asset.py
-    # result = rendimenti_asset.calcola_qualcosa()
-    return "<h1>Home QUANT-REA</h1>"
+    return "<h1>Home QUANT-REA</h1><p>Vai su /static/rendimenti_mensile.png per vedere un grafico.</p>"
+
+@app.route("/static/<filename>")
+def serve_image(filename):
+    return send_from_directory(os.path.join(app.root_path, "static"), filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
