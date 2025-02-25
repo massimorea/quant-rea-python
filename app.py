fd48aa8fbd0d01eaf4b5455a5888c4160cd3c754
massimorea <82827248+massimorea@users.noreply.github.com>
2025-02-25 01:34:04 +0100

    app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from dash import html, dcc
 from flask import Flask
 import os
 
-from ricerca import register_search_callbacks
-register_search_callbacks(app)
 
 
 # Inizializzazione del server Flask e Dash
