be564ddcd945cc41c22f0931807110cf67527664
massimorea <82827248+massimorea@users.noreply.github.com>
2025-02-25 01:25:19 +0100

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from dash import html, dcc
 from flask import Flask
 import os
 
+from ricerca import register_search_callbacks
+register_search_callbacks(app)
+
 # Inizializzazione del server Flask e Dash
 server = Flask(__name__)
 app = dash.Dash(__name__, server=server)
