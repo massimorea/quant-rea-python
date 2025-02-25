a2f736e57a28aaad6e4e0458cbfdfcc72b34c241
massimorea <82827248+massimorea@users.noreply.github.com>
2025-02-25 01:29:23 +0100

    rendimenti_volatilita.py

--- rendimenti_volatilita.py
+++ rendimenti_volatilita.py
@@ -6,7 +6,10 @@ import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
 from tvDatafeed import TvDatafeed, Interval
-from ricerca import get_search_layout, register_search_callbacks
+#from ricerca import get_search_layout, register_search_callbacks
+
+from ricerca import register_search_callbacks
+register_search_callbacks(app)
 
 # Inizializzazione dell'app Dash
 app = dash.Dash(__name__, server=False)
